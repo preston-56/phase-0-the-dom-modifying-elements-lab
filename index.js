93b95daf5c037cdec7055adf86bca6d2ca84a218
preston-56 <prestonosoro56@gmail.com>
2022-11-18 00:30:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+// Removes the childElement 
+const bodyElement = document.querySelector('body'); 
+const parentElement = document.getElementById("main"); 
+const childElement = bodyElement.removeChild(parentElement);
+
+// appendChild
+const newHeader = document.body.appendChild(document.createElement('h1'));
+
+// set id attribute
+
+newHeader.setAttribute('id', 'victory');
+
+// textContent
+newHeader.textContent = "Preston is the champion";   
